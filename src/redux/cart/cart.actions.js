98e68751ea4cb4496e3a55f8b4b1d8c03139878c
jblici <juambif@gmail.com>
2021-06-10 14:30:56 -0300

    updated cart utils to group items and shop data

--- src/redux/cart/cart.actions.js
+++ src/redux/cart/cart.actions.js
@@ -4,3 +4,12 @@ export const toggleCartHidden = () => ({
     type: CartActionTypes.TOGGLE_CART_HIDDEN
 })
 
+export const addItem = (item) => ({
+    type: CartActionTypes.ADD_ITEM,
+    payload: item
+})
+
+export const removeItem = (id) => ({
+    type: CartActionTypes.REMOVE_ITEM,
+    payload: id
+})
